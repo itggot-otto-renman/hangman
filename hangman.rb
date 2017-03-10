ea45355c002695fe879d0a55e34f4aa9e76ea0c8
itggot-otto-renman <otto.renman@itggot.se>
2017-03-10 12:22:06 +0100

    Blev klar med den
    xd

--- hangman.rb
+++ hangman.rb
@@ -1,43 +1,61 @@
 def hangman()
+  puts "Welcome to Hangman!"
+  puts "Try guessing the secret word!"
+  puts "Put only one letter"
   running = true
+  index = 0
   attempts = 5
   guessed = []
-  wordlist = File.readlines("myths-legends.txt")
+  wordlist = File.readlines("words-english.txt")
   word = wordlist.sample
   hidden = []
-  puts "Welcome to hangman!"
-  puts "Guess the word!"
-  puts "You have 5 attempts!"
   word.each_char do |x|
-    hidden.push("_")
+    hidden.push("_ ")
   end
   hidden.pop
+  word.upcase!
 
   while running
-    if guess == ""
-      puts "Please type a letter"
+    while hidden.include?("_ ") == true
+      puts "
+      "
+      guess = gets.chomp
+      while guess.length > 1
+        puts "Type only 1 letter"
+        guess = gets.chomp
+      end
+      guessed << guess
+      guess.upcase!
       index = 0
       if attempts == 0
-        puts "Really unfortunate. You lose!"
+        puts "You lose!"
         running = false
-      elsif hidden.include? ("_") == false
-        puts "Congratulations! You won! You had #{attempts} left"
-        running = false
-      else
-        guess = gets.chomp
-        guess.upcase
-        word.upcase
-        word.each_char do |x|
-          if guess == x
-            hidden[index] = guess
-            puts "Niiiiceeeee"
-
-            index += 1
-          end
-        end
       end
+      word.each_char do |x|
+        if guess == x
+          hidden[index] = guess + " "
+        else
+          attempts -= 1
+        end
+        index += 1
+      end
+      system "cls"
+      hidden.each do |z|
+        print z
+      end
+      puts "
+      "
+
+      guessed.to_s
+      puts "Guessed letters: #{guessed}"
+
     end
+    puts "
+
+    "
+    puts "Congtrats you win."
+    puts "You had #{attempts} attempts left!"
+    running = false
   end
 end
-
 hangman
