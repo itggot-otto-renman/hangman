6fb255a52cae4b867813d5982860fac8d9b9a91e
itggot-otto-renman <otto.renman@itggot.se>
2017-03-15 11:01:35 +0100

    Allt fungerar förutom en del saker
    Nästa lektion: Fixa attemts, Fixa så att hidden fungerar på rätt vis,
    Fixa index placering

--- hangman.rb
+++ hangman.rb
@@ -5,46 +5,65 @@ def hangman()
   puts "Put only one letter"
   puts "You have 5 attempts left"
   running = true
+  rightguess = true
   index = 0
   attempts = 5
   guessed = []
   wordlist = File.readlines("myths-legends.txt")
   word = wordlist.sample
+  word = word.chomp
   hidden = []
   word.each_char do |x|
     hidden.push("_ ")
   end
-  hidden.pop
-  word.upcase!
+  word = word.upcase!
+  hidden.each do |z|
+    print z
+  end
 
   while running
     while hidden.include?("_ ") == true
       puts "
       "
       guess = gets.chomp
+      while guess == "" || guess ==" "
+        puts "Please type a letter"
+        guess = gets.chomp
+      end
       while guess.length > 1
         puts "Type only 1 letter"
         guess = gets.chomp
       end
       guessed << guess
       guess.upcase!
-      index = 0
-      if attempts == 0
+      if attempts == 1
         puts "Unfortunate, you lose!"
         running = false
+        break
       end
+
+
       word.each_char do |x|
         if guess == x
           hidden[index] = guess + " "
+          rightguess = true
+          index += 1
         else
-          attempts -= 1
-          
+          rightguess = false
         end
-        index += 1
       end
-      system "cls"
+      if rightguess != true
+        attempts -= 1
+      end
+      # system "cls"
       hidden.each do |z|
         print z
+      end
+      if hidden.include?("_ ") == false
+        puts "Congratulations! You won!"
+        puts "You had #{attempts} attempts left!"
+        running = false
+        break
       end
       puts "
       "
@@ -52,14 +71,7 @@ def hangman()
       guessed.to_s
       puts "You have #{attempts} attempts left!"
       puts "Guessed letters: #{guessed}"
-
     end
-    puts "
-
-    "
-    puts "Congratulations! You won!"
-    puts "You had #{attempts} attempts left!"
-    running = false
   end
 end
 hangman
